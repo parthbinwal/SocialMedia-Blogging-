0ec5adf1232b150fd5b8dbbc861646c5abe8506a
Parth Binwal <binwalparth@gmail.com>
2025-09-07 20:02:38 +0100

    creating routes for comment

--- Backend/controllers/comments/commentsController.js
+++ Backend/controllers/comments/commentsController.js
@@ -4,9 +4,14 @@ const Post=require('../../models/Posts/Post');
 //create a new comment
 //post private /api/v1/comments/:postid;
 exports.createComment=asyncHandler(async(req,resp)=>{
+  const {message}=req.body;
+  const postId=req.params.postId;
+  const comment=await Comment.create({message,authorId:req?.userAuth?._id,postId})
+  await Post.findByIdAndUpdate(postId,{$push:{comments:comment._id}},{new:true})
+  resp.status(201).json({
+    status:"success",
+    message:"comment sucessfully created",
+    comment
+  })
 
-
-
-
-  
 })
